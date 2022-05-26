b9d5da3729d273cdf1d304bea7972a4da9aa5cf8
jefferson <jeffsilva2@gmail.com>
2022-05-26 03:58:25 -0300

    Criando a base de dados comercio

---
+++ database-comercio01.sql
@@ -0,0 +1,12 @@
+# Criando o nosso banco de dados chamado comercial
+create database comercial;
+show databases;
+use comercial;
+
+# Criando a tabela cliente
+create table comclien(
+	n_numeclien int not null auto_increment,
+    c_codiclien varchar(10),
+    
+    
+);
