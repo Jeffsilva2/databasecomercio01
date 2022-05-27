cfda71ec77449484961c493501a49fdbf8c77c30
jefferson <jeffsilva2@gmail.com>
2022-05-27 00:09:22 -0300

    Foi criada a tabela comclien

--- database-comercio01.sql
+++ database-comercio01.sql
@@ -8,5 +8,11 @@ create table comclien(
 	n_numeclien int not null auto_increment,
     c_codiclien varchar(10),
     c_nomeclien varchar(100),
+    c_razaclien varchar(100),
+    d_dataclien date,
+	c_cnpjclien varchar(20),
+	c_foneclien varchar(20),
+	primary key (n_numeclien));
     
-);
+    # Utilizando o describe para verificar se a tabela realmente foi criada
+    desc comclien
